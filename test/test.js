b1111b589402e9a7256ba79994fad4b5c37a38e1
Nico Hauser <me@tyratox.ch>
2017-09-03 11:39:14 +0200

    Fixed test path

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ import test from "ava";
 
 process.on("uncaughtException", console.log);
 
-const Lazuli = require("../src/lazuli");
+const Lazuli = require("../src/lazuli-core");
 
 test("load lazuli", t => {
 	t.pass();
