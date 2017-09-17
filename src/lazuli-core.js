6e5c16871a2839601abf56838ff0edd47afe557a
Nico Hauser <me@tyratox.ch>
2017-09-17 17:15:33 +0200

    Removed graphql-errors; seems to not work

--- src/lazuli-core.js
+++ src/lazuli-core.js
@@ -8,8 +8,6 @@ const sequelize = require("../globals/sequelize");
 const { GraphQLSchema, GraphQLObjectType } = require("graphql");
 const graphqlHTTP = require("express-graphql");
 
-const { maskErrors } = require("graphql-errors");
-
 /**
  * The lazuli class
  * @type {Lazuli}
@@ -60,28 +58,26 @@ Lazuli.prototype.init = function() {
 				"/graphql",
 				graphqlHTTP(request => {
 					return {
-						schema: maskErrors(
-							new GraphQLSchema({
-								query: new GraphQLObjectType({
-									name: "RootQuery",
-									fields: valueFilter.filterable(
-										"graphql.schema.root.query.fields",
-										{ node: sequelize.nodeField },
-										sequelize,
-										models
-									)
-								}),
-								mutation: new GraphQLObjectType({
-									name: "RootMutation",
-									fields: valueFilter.filterable(
-										"graphql.schema.root.mutation.fields",
-										{},
-										sequelize,
-										models
-									)
-								})
+						schema: new GraphQLSchema({
+							query: new GraphQLObjectType({
+								name: "RootQuery",
+								fields: valueFilter.filterable(
+									"graphql.schema.root.query.fields",
+									{ node: sequelize.nodeField },
+									sequelize,
+									models
+								)
+							}),
+							mutation: new GraphQLObjectType({
+								name: "RootMutation",
+								fields: valueFilter.filterable(
+									"graphql.schema.root.mutation.fields",
+									{},
+									sequelize,
+									models
+								)
 							})
-						),
+						}),
 						context: request,
 						graphiql: true
 					};
