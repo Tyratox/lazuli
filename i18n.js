dc947808114f120706078960eaa2c4c207d6a2ec
Nico Hauser <me@tyratox.ch>
2017-11-01 19:20:36 +0100

    Movied i18n to the core

---
+++ i18n.js
@@ -0,0 +1,18 @@
+const { LOCALE_PATH, LOCALES } = require("lazuli-require")("lazuli-config");
+
+/**
+ * The configured i18n object
+ * @type {object}
+ */
+const i18n = require("i18n");
+
+i18n.configure({
+	locales: LOCALES,
+	defaultLocale: LOCALES[0],
+	directory: LOCALE_PATH,
+	autoReload: true,
+	extension: ".json",
+	prefix: ""
+});
+
+module.exports = i18n;
