40cacf87e9f670afcf0a0ad0db4fd12ff5c590ea
Nico Hauser <me@tyratox.ch>
2017-11-04 10:55:24 +0100

    Restructured logger

--- logger.js
+++ logger.js
@@ -1,41 +1,6 @@
 const { LOG_FILE } = require("lazuli-require")("lazuli-config");
 
 const winston = require("winston");
-const transports = [];
-
-transports.push(
-	new winston.transports.Console({
-		name: "console-log",
-		level: "info",
-		colorize: true,
-		prettyPrint: true
-	})
-);
-transports.push(
-	new winston.transports.File({
-		name: "file-log",
-		level: "debug",
-		filename: LOG_FILE,
-		handleExceptions: true,
-		colorize: false,
-		prettyPrint: true
-	})
-);
-
-/**
- * The configured winston logger
- * @type {Logger}
- */
-const logger = new winston.Logger({
-	transports,
-	levels: {
-		critical: 0,
-		error: 1,
-		warning: 2,
-		info: 3,
-		debug: 4
-	}
-});
 
 winston.addColors({
 	critical: "red",
@@ -43,6 +8,36 @@ winston.addColors({
 	warning: "yellow",
 	info: "blue",
 	debug: "magenta"
+});
+
+/**
+ * The configured winston logger
+ * @type {Logger}
+ */
+const logger = new winston.Logger({
+	transports: [
+		new winston.transports.Console({
+			name: "console",
+			level: "info",
+			colorize: true,
+			prettyPrint: true
+		}),
+		new winston.transports.File({
+			name: "file",
+			level: "debug",
+			filename: LOG_FILE,
+			handleExceptions: true,
+			colorize: false,
+			prettyPrint: true
+		})
+	],
+	levels: {
+		critical: 0,
+		error: 1,
+		warning: 2,
+		info: 3,
+		debug: 4
+	}
 });
 
 module.exports = logger;
