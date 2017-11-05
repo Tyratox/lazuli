852538a76cb6134936a55aafa6ac408fc1dd8c5b
Nico Hauser <me@tyratox.ch>
2017-11-05 14:26:37 +0100

    Disabled operatorsAliases

--- sequelize.js
+++ sequelize.js
@@ -38,7 +38,8 @@ let sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
 		min: DB_POOL_MIN,
 		idle: DB_POOL_IDLE
 	},
-	logging: false //s => logger.log("debug", s)
+	logging: false, //s => logger.log("debug", s)
+	operatorsAliases: false
 });
 
 sequelize
